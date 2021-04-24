1e9c59a9a622bc5ce5948508890ca7e83e8f570a
GraySnowFox <78774887+GraySnowFox@users.noreply.github.com>
2021-04-24 00:56:07 -0500

    Names functioning

--- script.js
+++ script.js
@@ -2,13 +2,27 @@ window.addEventListener("load", function(){
 
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then(function(json){
+
+
+            
             console.log(json.length);
             let container = document.getElementById("container");
             for(let i = 0; i<json.length; i++){
 
+                //get json data
+                let first = json[i].firstName;
+                console.log(first);
+
+
+
+
+
+
+
+
                 container.innerHTML += "<div class='astronaut'>" +
                     "<div class='bio'>" +
-                        "<h3>${json.firstName) $(json.lastName}</h3>" +
+                        "<h3>" + json[i].firstName +" " +json[i].lastName+ "</h3>" +
                         "<ul>" +
                             "<li>Hours in space: ${json.hoursInSpace}</li>" +
                             "<li>Active: ${json.active}</li>" +
